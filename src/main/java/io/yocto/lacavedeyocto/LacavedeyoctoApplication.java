07b3e398486462dba33604fda29c976c8ce624f1
Thomas Bulens <bulenst@gmail.com>
2024-03-14 12:08:37 +0100

    first setup

---
+++ src/main/java/io/yocto/lacavedeyocto/LacavedeyoctoApplication.java
@@ -0,0 +1,13 @@
+package io.yocto.lacavedeyocto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacavedeyoctoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacavedeyoctoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yocto/lacavedeyocto/LacavedeyoctoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yocto.lacavedeyocto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacavedeyoctoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
